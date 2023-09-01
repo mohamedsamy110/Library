93c5cab8340c2411aa38a4feb3117e54d3b5fd13
mohamedsamy110 <143813409+mohamedsamy110@users.noreply.github.com>
2023-09-01 16:48:37 +0300

    Add files via upload

---
+++ Addbook.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace day16.ViewModel
+{
+    public class Addbook
+    {
+        [Required(ErrorMessage ="Name Is Required")]
+        [MinLength(3,ErrorMessage ="name must be 3 char min")]
+        public string Name { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+
+
+
+    }
+}
---
+++ TestViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace day16.ViewModel
+{
+    public class TestViewModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public int AuthorId { get; set; }
+
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
